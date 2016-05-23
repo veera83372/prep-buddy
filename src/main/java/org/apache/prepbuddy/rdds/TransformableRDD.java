b94e55f4f26807210af8d6bb5a13044c296adb53
Lalit Pandey <lalitpan@thoughtworks.com>
2016-05-23 11:06:04 +0530

    refactoring

--- src/main/java/org/apache/prepbuddy/rdds/TransformableRDD.java
+++ src/main/java/org/apache/prepbuddy/rdds/TransformableRDD.java
@@ -257,6 +257,8 @@ public class TransformableRDD extends JavaRDD<String> {
                 String[] recordAsArray = fileType.parseRecord(row);
                 String columnValue = recordAsArray[columnIndex];
                 String otherColumnValue = recordAsArray[xColumnIndex];
+                System.out.println("otherColumnValue = " + otherColumnValue);
+                System.out.println("columnValue = " + columnValue);
                 if (columnValue.trim().isEmpty() || otherColumnValue.trim().isEmpty())
                     return 0;
                 return Double.parseDouble(recordAsArray[columnIndex]) * Double.parseDouble(recordAsArray[xColumnIndex]);
