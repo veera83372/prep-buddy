37d71edeeb3d0a6fa5334eed9462f2345a628684
Lalit Pandey <lalitpan@thoughtworks.com>
2016-06-06 10:30:34 +0530

    Wrote one test for documentation

--- test/main/java/org/apache/prepbuddy/datacleansers/imputation/NaiveBayesSubstitutionTest.java
+++ test/main/java/org/apache/prepbuddy/datacleansers/imputation/NaiveBayesSubstitutionTest.java
@@ -44,4 +44,27 @@ public class NaiveBayesSubstitutionTest extends SparkTestCase {
         assertEquals("P", naiveBayesSubstitution.handleMissingData(new RowRecord(record)));
 
     }
+
+    @Test
+    public void shouldPredictGender() {
+        JavaRDD<String> initialDataSet = javaSparkContext.parallelize(Arrays.asList(
+                "Drew,No,Blue,Short,Male",
+                "Claudia,Yes,Brown,Long,Female",
+                "Drew,No,Blue,Long,Female",
+                "Drew,No,Blue,Long,Female",
+                "Alberto,Yes,Brown,Short,Male",
+                "Karin,No,Blue,Long,Female",
+                "Nina,Yes,Brown,Short,Female",
+                "Sergio,Yes,Blue,Long,Male"
+        ));
+        TransformableRDD initialRDD = new TransformableRDD(initialDataSet);
+
+        NaiveBayesSubstitution naiveBayesSubstitution = new NaiveBayesSubstitution(0, 1, 2, 3);
+        naiveBayesSubstitution.prepareSubstitute(initialRDD, 4);
+        String[] rowRecord = ("Drew,Yes,Blue,Long,").split(",");
+        String mostProbable = naiveBayesSubstitution.handleMissingData(new RowRecord(rowRecord));
+
+        assertEquals("Female", mostProbable);
+
+    }
 }
