555b5b120ac9e83e70ea024c310d21320eb4e909
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-06-17 12:33:36 +0530

    [Abhishek] removed the java_import statments. Accessing it directly by full Class name

--- python/py_prep_buddy/transformableRDD.py
+++ python/py_prep_buddy/transformableRDD.py
@@ -1,4 +1,3 @@
-from py4j.java_gateway import java_import
 from pyspark import RDD
 
 from buddySerializer import BuddySerializer
@@ -10,24 +9,21 @@ class TransformableRDD(RDD):
             jvm = rdd.ctx._jvm
             self.__set_file_type(jvm, file_type)
             self.ctx = rdd.ctx
-            java_import(jvm, 'org.apache.prepbuddy.rdds.TransformableRDD')
-            java_import(jvm, 'org.apache.prepbuddy.pythonConnector.*')
-            java_rdd = rdd._reserialize(BuddySerializer())._jrdd.map(jvm.BytesToString())
-            self._t_rdd = jvm.TransformableRDD(java_rdd, self.__file_type)
+            java_rdd = rdd._reserialize(BuddySerializer())._jrdd.map(
+                    jvm.org.apache.prepbuddy.pythonConnector.BytesToString())
+            self._t_rdd = jvm.org.apache.prepbuddy.rdds.TransformableRDD(java_rdd, self.__file_type)
             RDD.__init__(self, rdd._jrdd, rdd.ctx)
         else:
             jvm = sc._jvm
             self.__file_type = file_type
-            java_import(jvm, 'org.apache.prepbuddy.pythonConnector.*')
             self._t_rdd = t_rdd
-            rdd = t_rdd.map(jvm.StringToBytes())
+            rdd = t_rdd.map(jvm.org.apache.prepbuddy.pythonConnector.StringToBytes())
             RDD.__init__(self, rdd, sc, BuddySerializer())
 
     def __set_file_type(self, jvm, file_type):
-        java_import(jvm, "org.apache.prepbuddy.typesystem.*")
         file_types = {
-            'CSV': jvm.FileType.CSV,
-            'TSV': jvm.FileType.TSV
+            'CSV': jvm.org.apache.prepbuddy.typesystem.FileType.CSV,
+            'TSV': jvm.org.apache.prepbuddy.typesystem.FileType.TSV
         }
 
         if file_type.upper() in file_types:
