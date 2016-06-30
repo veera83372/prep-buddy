c29ba06e739530840f694ccfc4292ca544162946
Rahul <rahulna@thoughtworks.com>
2016-06-30 14:52:12 +0530

    Resolved Merge

--- src/main/scala/org/apache/datacommons/prepbuddy/rdds/TransformableRDD.scala
+++ src/main/scala/org/apache/datacommons/prepbuddy/rdds/TransformableRDD.scala
@@ -11,8 +11,6 @@ import scala.collection.mutable
 
 class TransformableRDD(parent: RDD[String], fileType: FileType = CSV) extends RDD[String](parent) {
 
-class TransformableRDD(parent: RDD[String], fileType: FileType = CSV) extends RDD[String](parent) {
-
     def dropColumn(columnIndex: Int): TransformableRDD = {
         val transformed: RDD[String] = this.map((record: String) => {
             val recordInBuffer: mutable.Buffer[String] = fileType.parseRecord(record).toBuffer
@@ -23,35 +21,32 @@ class TransformableRDD(parent: RDD[String], fileType: FileType = CSV) extends RD
     }
 
 
+    def deduplicate(): TransformableRDD = {
 
-  def deduplicate(): TransformableRDD = {
+        val mappedRDD: RDD[(Long, String)] = this.map((record) => {
+            val columns: Array[String] = this.fileType.parseRecord(record)
+            val fingerprint = generateFingerPrint(columns)
+            (fingerprint, record)
+        })
 
-    val mappedRDD: RDD[(Long, String)] = this.map((record) => {
-      val columns: Array[String] = this.fileType.parseRecord(record)
-      val fingerprint = generateFingerPrint(columns)
-      (fingerprint, record)
-    })
+        val reducedRDD: RDD[(Long, String)] = mappedRDD.reduceByKey((accumulator, record) => {
+            record
+        })
+        new TransformableRDD(reducedRDD.values, fileType)
+    }
 
-    val reducedRDD: RDD[(Long, String)] = mappedRDD.reduceByKey((accumulator, record) => {
-      record
-    })
-    new TransformableRDD(reducedRDD.values, fileType)
-  }
-
-  private def generateFingerPrint(columns: Array[String]): Long = {
-    val concatenatedString: String = columns.mkString("")
-    val algorithm: MessageDigest = MessageDigest.getInstance("MD5")
-    algorithm.update(concatenatedString.getBytes, 0, concatenatedString.length)
-    BigInt(algorithm.digest()).longValue()
-  }
+    private def generateFingerPrint(columns: Array[String]): Long = {
+        val concatenatedString: String = columns.mkString("")
+        val algorithm: MessageDigest = MessageDigest.getInstance("MD5")
+        algorithm.update(concatenatedString.getBytes, 0, concatenatedString.length)
+        BigInt(algorithm.digest()).longValue()
+    }
 
 
-  @DeveloperApi
-  override def compute(split: Partition, context: TaskContext): Iterator[String] = {
-    parent.compute(split, context)
-  }
-
-  override protected def getPartitions: Array[Partition] = parent.partitions
+    @DeveloperApi
+    override def compute(split: Partition, context: TaskContext): Iterator[String] = {
+        parent.compute(split, context)
+    }
 
     override protected def getPartitions: Array[Partition] = parent.partitions
 }
--- src/test/scala/org/apache/datacommons/prepbuddy/rdds/TransformableRDDTest.scala
+++ src/test/scala/org/apache/datacommons/prepbuddy/rdds/TransformableRDDTest.scala
@@ -10,36 +10,29 @@ import org.scalatest.{BeforeAndAfterEach, FunSuite}
 
 class TransformableRDDTest extends FunSuite with BeforeAndAfterEach {
 
-  var sparkContext: SparkContext = null
+    var sparkContext: SparkContext = null
 
-  override def beforeEach() {
-    val sparkConf: SparkConf = new SparkConf().setAppName(getClass.getName).setMaster("local")
-    sparkContext = new SparkContext(sparkConf)
-    Logger.getLogger("org").setLevel(Level.OFF)
-    Logger.getLogger("akka").setLevel(Level.OFF)
-  }
+    override def beforeEach() {
+        val sparkConf: SparkConf = new SparkConf().setAppName(getClass.getName).setMaster("local")
+        sparkContext = new SparkContext(sparkConf)
+        Logger.getLogger("org").setLevel(Level.OFF)
+        Logger.getLogger("akka").setLevel(Level.OFF)
+    }
 
-  override def afterEach() {
-    sparkContext.stop()
-  }
+    override def afterEach() {
+        sparkContext.stop()
+    }
 
-  test("should be able to count on transformableRdd") {
-    val data = Array("1,23", "2,45", "3,65", "4,67", "5,23")
-    val dataSet: RDD[String] = sparkContext.parallelize(data)
-    val transformableRDD: TransformableRDD = new TransformableRDD(dataSet, CSV)
-    assert(5 == transformableRDD.count())
-  }
-
-  test("should deduplicate a dataset") {
-    val records: Array[String] = Array("Smith,Male,USA,12345", "John,Male,USA,12343", "John,Male,USA,12343", "Smith,Male,USA,12342", "John,Male,India,12343", "Smith,Male,USA,12342")
-    val initialDataset: JavaRDD[String] = sparkContext.parallelize(records)
-    val initialRDD: TransformableRDD = new TransformableRDD(initialDataset)
-    val deduplicatedRDD: TransformableRDD = initialRDD.deduplicate()
-    assertEquals(4, deduplicatedRDD.count)
-  }
+    test("should deduplicate a dataset") {
+        val records: Array[String] = Array("Smith,Male,USA,12345", "John,Male,USA,12343", "John,Male,USA,12343", "Smith,Male,USA,12342", "John,Male,India,12343", "Smith,Male,USA,12342")
+        val initialDataset: RDD[String] = sparkContext.parallelize(records)
+        val initialRDD: TransformableRDD = new TransformableRDD(initialDataset)
+        val deduplicatedRDD: TransformableRDD = initialRDD.deduplicate()
+        assertEquals(4, deduplicatedRDD.count)
+    }
     test("should be able to count on transformableRdd") {
         val data = Array("1,23", "2,45", "3,65", "4,67", "5,23")
-        val dataSet: RDD[String] = sc.parallelize(data)
+        val dataSet: RDD[String] = sparkContext.parallelize(data)
         val transformableRDD: TransformableRDD = new TransformableRDD(dataSet, CSV)
         assert(5 == transformableRDD.count())
     }
@@ -51,7 +44,7 @@ class TransformableRDDTest extends FunSuite with BeforeAndAfterEach {
             "Larry, Male, 23, USA",
             "Fiona, Female,18,USA"
         )
-        val dataset: RDD[String] = sc.parallelize(data)
+        val dataset: RDD[String] = sparkContext.parallelize(data)
         val transformableRDD: TransformableRDD = new TransformableRDD(dataset, CSV)
         val transformedRows: Array[String] = transformableRDD.dropColumn(2).collect()
 
