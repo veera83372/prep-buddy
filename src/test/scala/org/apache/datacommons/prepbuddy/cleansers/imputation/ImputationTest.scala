d21a6422426b9f9911b8e67e55ac59bd865e7241
Lalit Pandey <lalitpan@thoughtworks.com>
2016-06-30 16:48:55 +0530

    Added imputation by Approx mean

--- src/test/scala/org/apache/datacommons/prepbuddy/cleansers/imputation/ImputationTest.scala
+++ src/test/scala/org/apache/datacommons/prepbuddy/cleansers/imputation/ImputationTest.scala
@@ -45,4 +45,14 @@ class ImputationTest extends FunSuite with BeforeAndAfterEach  {
     val expected: String = "1,40.0"
     assert(collected.contains(expected))
   }
+
+  test("should impute the missing values by approx mean") {
+    val data = Array("1,", "2,45", "3,65", "4,67", "5,23")
+    val dataSet: RDD[String] = sparkContext.parallelize(data)
+    val transformableRDD: TransformableRDD = new TransformableRDD(dataSet, CSV)
+    val imputedByMean: TransformableRDD = transformableRDD.impute(1, new ApproxMeanSubstitution())
+    val collected: Array[String] = imputedByMean.collect()
+    val expected: String = "1,40.0"
+    assert(collected.contains(expected))
+  }
 }
