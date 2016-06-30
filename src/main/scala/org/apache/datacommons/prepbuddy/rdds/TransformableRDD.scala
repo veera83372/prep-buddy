6b51294c5fd36e0d402e003bce1dbe0d2c3f9925
srihari_srinivasan <ssriniva@thoughtworks.com>
2016-06-30 13:02:32 +0530

    Implemented Deduplication based on all columns

--- src/main/scala/org/apache/datacommons/prepbuddy/rdds/TransformableRDD.scala
+++ src/main/scala/org/apache/datacommons/prepbuddy/rdds/TransformableRDD.scala
@@ -1,15 +1,37 @@
 package org.apache.datacommons.prepbuddy.rdds
 
+import java.security.MessageDigest
+
 import org.apache.datacommons.prepbuddy.types.{CSV, FileType}
 import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{Partition, TaskContext}
 
 
-class TransformableRDD(parent: RDD[String], fileType: FileType = CSV)
-  extends RDD[String](parent) {
+class TransformableRDD(parent: RDD[String], fileType: FileType = CSV) extends RDD[String](parent) {
 
-  override def filter(f: (String) => Boolean): TransformableRDD = new TransformableRDD(super.filter(f),fileType)
+
+  def deduplicate(): TransformableRDD = {
+
+    val mappedRDD: RDD[(Long, String)] = this.map((record) => {
+      val columns: Array[String] = this.fileType.parseRecord(record)
+      val fingerprint = generateFingerPrint(columns)
+      (fingerprint, record)
+    })
+
+    val uniqueValuesRDD: RDD[(Long, String)] = mappedRDD.reduceByKey((accumulator, record) => {
+      record
+    })
+    new TransformableRDD(uniqueValuesRDD.values, fileType)
+  }
+
+  private def generateFingerPrint(columns: Array[String]): Long = {
+    val concatenatedString: String = columns.mkString("")
+    val algorithm: MessageDigest = MessageDigest.getInstance("MD5")
+    algorithm.update(concatenatedString.getBytes, 0, concatenatedString.length)
+    BigInt(algorithm.digest()).longValue()
+  }
+
 
   @DeveloperApi
   override def compute(split: Partition, context: TaskContext): Iterator[String] = {
--- src/test/scala/org/apache/datacommons/prepbuddy/rdds/TransformableRDDTest.scala
+++ src/test/scala/org/apache/datacommons/prepbuddy/rdds/TransformableRDDTest.scala
@@ -2,29 +2,40 @@ package org.apache.datacommons.prepbuddy.rdds
 
 import org.apache.datacommons.prepbuddy.types.CSV
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import org.junit.Assert._
 import org.scalatest.{BeforeAndAfterEach, FunSuite}
 
 
-class TransformableRDDTest extends FunSuite with BeforeAndAfterEach{
+class TransformableRDDTest extends FunSuite with BeforeAndAfterEach {
 
-  var sc :SparkContext = null
+  var sparkContext: SparkContext = null
+
   override def beforeEach() {
     val sparkConf: SparkConf = new SparkConf().setAppName(getClass.getName).setMaster("local")
-    sc = new SparkContext(sparkConf)
+    sparkContext = new SparkContext(sparkConf)
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
   }
 
   override def afterEach() {
-    sc.stop()
+    sparkContext.stop()
   }
 
   test("should be able to count on transformableRdd") {
     val data = Array("1,23", "2,45", "3,65", "4,67", "5,23")
-    val dataSet: RDD[String] = sc.parallelize(data)
-    val transformableRDD: TransformableRDD = new TransformableRDD(dataSet,CSV)
-    assert(5==transformableRDD.count())
+    val dataSet: RDD[String] = sparkContext.parallelize(data)
+    val transformableRDD: TransformableRDD = new TransformableRDD(dataSet, CSV)
+    assert(5 == transformableRDD.count())
+  }
+
+  test("should deduplicate a dataset") {
+    val records: Array[String] = Array("Smith,Male,USA,12345", "John,Male,USA,12343", "John,Male,USA,12343", "Smith,Male,USA,12342", "John,Male,India,12343", "Smith,Male,USA,12342")
+    val initialDataset: JavaRDD[String] = sparkContext.parallelize(records)
+    val initialRDD: TransformableRDD = new TransformableRDD(initialDataset)
+    val deduplicatedRDD: TransformableRDD = initialRDD.deduplicate()
+    assertEquals(4, deduplicatedRDD.count)
   }
 }
