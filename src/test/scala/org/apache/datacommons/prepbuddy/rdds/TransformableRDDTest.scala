55b89c5cf289fba8e13da1c41e90a7a679c8485b
Rahul <rahulna@thoughtworks.com>
2016-07-01 15:37:04 +0530

    All duplication related tests are moved to seperate class called DuplicationTest

--- src/test/scala/org/apache/datacommons/prepbuddy/rdds/TransformableRDDTest.scala
+++ src/test/scala/org/apache/datacommons/prepbuddy/rdds/TransformableRDDTest.scala
@@ -17,53 +17,6 @@ class TransformableRDDTest extends SparkTestCase {
         assert(5 == transformableRDD.count())
     }
 
-    test("should deduplicate a dataset by considering all the columns") {
-        val records: Array[String] = Array(
-            "Smith,Male,USA,12345",
-            "John,Male,USA,12343",
-            "John,Male,USA,12343",
-            "Smith,Male,USA,12342",
-            "John,Male,India,12343",
-            "Smith,Male,USA,12342"
-        )
-        val initialDataset: RDD[String] = sparkContext.parallelize(records)
-        val initialRDD: TransformableRDD = new TransformableRDD(initialDataset)
-        val deduplicatedRDD: TransformableRDD = initialRDD.deduplicate()
-        assert(4 == deduplicatedRDD.count)
-    }
-
-    test("should deduplicate a dataset by considering the given columns as primary key") {
-        val records: Array[String] = Array(
-            "Smith,Male,USA,12345",
-            "John,Male,USA,12343",
-            "John,Male,USA,12343",
-            "Smith,Male,USA,12342",
-            "John,Male,India,12343",
-            "Smith,Male,USA,12342"
-        )
-        val initialDataset: RDD[String] = sparkContext.parallelize(records)
-        val initialRDD: TransformableRDD = new TransformableRDD(initialDataset)
-        val deduplicatedRDD: TransformableRDD = initialRDD.deduplicate(List(0, 1))
-
-        assertEquals(2, deduplicatedRDD.count)
-    }
-
-    test("should detect duplicates from a dataset by considering the given columns as primary key") {
-        val records: Array[String] = Array(
-            "Smith,Male,USA,12345",
-            "John,Male,USA,12343",
-            "John,Male,UK,12343",
-            "Smith,Male,USA,12342",
-            "John,Male,India,12343",
-            "Smith,Male,USA,12342"
-        )
-        val initialDataset: RDD[String] = sparkContext.parallelize(records)
-        val initialRDD: TransformableRDD = new TransformableRDD(initialDataset)
-        val deduplicatedRDD: TransformableRDD = initialRDD.duplicates(List(0, 1, 3))
-
-        assertEquals(4, deduplicatedRDD.count)
-    }
-
     test("should drop the specified column from the given rdd") {
         val data = Array(
             "John,Male,21,Canada",
