a792fe1f0e33c58357a0ef9523830e812ecafa0d
Lalit Pandey <lalitpan@thoughtworks.com>
2016-07-08 11:38:15 +0530

    refactoring

--- src/test/scala/org/apache/datacommons/prepbuddy/ExceptionTest.scala
+++ src/test/scala/org/apache/datacommons/prepbuddy/ExceptionTest.scala
@@ -1,13 +1,13 @@
 package org.apache.datacommons.prepbuddy
 
 import org.apache.datacommons.prepbuddy.exceptions.ApplicationException
-import org.apache.datacommons.prepbuddy.smoothers.{WeightedMovingAverageMethod, Weights}
+import org.apache.datacommons.prepbuddy.smoothers.{WeightedSlidingWindow, WeightedMovingAverageMethod, Weights}
 
 class ExceptionTest extends SparkTestCase{
     test("Weighted moving average should not creatable when weights sze and window size is not equal") {
         val weights: Weights = new Weights(3)
         val thrown = intercept[ApplicationException] {
-            new WeightedMovingAverageMethod(1, weights)
+            new WeightedSlidingWindow(1, weights)
         }
         assert(thrown.getMessage == "Window size and weighs size should be same")
     }
