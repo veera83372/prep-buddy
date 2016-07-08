8bce425d125dd95c2f9b3dd61c49f260515819f8
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-07-08 17:22:26 +0530

    Checking the integrity of a particular data type.   Added a check that if the type is not determinable it should give ALPHANUMERIC as default.

--- src/main/scala/org/apache/datacommons/prepbuddy/qualityanalyzers/BaseDataType.scala
+++ src/main/scala/org/apache/datacommons/prepbuddy/qualityanalyzers/BaseDataType.scala
@@ -5,15 +5,16 @@ abstract class BaseDataType extends Serializable {
 
     protected def checkActualType(sampleData: List[String], subtypes: Array[DataType]): DataType = {
         val tuples: Array[(DataType, Int)] = subtypes.map(dataType => (dataType, dataType.matchingCount(sampleData)))
-        val reduce: (DataType, Int) = tuples.reduce((tuple, another) => {
+        val max: (DataType, Int) = tuples.reduce((tuple, another) => {
             if (tuple._2 > another._2) tuple else another
         })
-        reduce._1
+        val threshold = Math.round(sampleData.length * 0.75)
+        if (max._2 < threshold) ALPHANUMERIC_STRING else max._1
     }
 }
 
 object STRING extends BaseDataType {
-    val subtypes: Array[DataType] = Array(EMAIL, ALPHANUMERIC_STRING)
+    val subtypes: Array[DataType] = Array(EMAIL, CURRENCY)
 
     override def actualType(sampleData: List[String]): DataType = {
         super.checkActualType(sampleData, subtypes)
