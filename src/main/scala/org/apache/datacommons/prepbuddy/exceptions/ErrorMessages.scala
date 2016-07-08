ad31d2b3c1c7ea6154fca84c04fefe5689ca34dd
Lalit Pandey <lalitpan@thoughtworks.com>
2016-07-08 18:22:52 +0530

    rearranged error messages

--- src/main/scala/org/apache/datacommons/prepbuddy/exceptions/ErrorMessages.scala
+++ src/main/scala/org/apache/datacommons/prepbuddy/exceptions/ErrorMessages.scala
@@ -2,22 +2,34 @@ package org.apache.datacommons.prepbuddy.exceptions
 
 object ErrorMessages {
     val WEIGHTS_SUM_IS_NOT_EQUAL_TO_ONE: ErrorMessage = {
-        new ErrorMessage("WEIGHTS_SUM_IS_NOT_EQUAL_TO_ONE", "To calculate weighted moving average weights sum should be up to one.")
+        val message: String = "To calculate weighted moving average weights sum should be up to one."
+        val key: String = "WEIGHTS_SUM_IS_NOT_EQUAL_TO_ONE"
+        new ErrorMessage(key, message)
     }
     val WINDOW_SIZE_AND_WEIGHTS_SIZE_NOT_MATCHING: ErrorMessage = {
-        new ErrorMessage("WINDOW_SIZE_AND_WEIGHTS_SIZE_NOT_MATCHING", "Window size and weighs size should be same.")
+        val message: String = "Window size and weighs size should be same."
+        val key: String = "WINDOW_SIZE_AND_WEIGHTS_SIZE_NOT_MATCHING"
+        new ErrorMessage(key, message)
     }
     val SIZE_LIMIT_IS_EXCEEDED: ErrorMessage = {
-        new ErrorMessage("SIZE_LIMIT_IS_EXCEEDED", "Can not add value more than size limit.")
+        val key: String = "SIZE_LIMIT_IS_EXCEEDED"
+        val message: String = "Can not add value more than size limit."
+        new ErrorMessage(key, message)
     }
 
     val COLUMN_INDEX_OUT_OF_BOUND: ErrorMessage = {
-        new ErrorMessage("COLUMN_INDEX_OUT_OF_BOUND", "Column index is out of bound.")
+        val key: String = "COLUMN_INDEX_OUT_OF_BOUND"
+        val message: String = "Column index is out of bound."
+        new ErrorMessage(key, message)
     }
     val NEGATIVE_COLUMN_INDEX: ErrorMessage = {
-        new ErrorMessage("NEGATIVE_COLUMN_INDEX", "Column index can not be negative.")
+        val key: String = "NEGATIVE_COLUMN_INDEX"
+        val message: String = "Column index can not be negative."
+        new ErrorMessage(key, message)
     }
     val PROBABILITY_IS_NOT_IN_RANGE: ErrorMessage = {
-        new ErrorMessage("PROBABILITY_IS_NOT_IN_RANGE", "Probability can not be less than zero or greater than 1")
+        val key: String = "PROBABILITY_IS_NOT_IN_RANGE"
+        val message: String = "Probability can not be less than zero or greater than 1"
+        new ErrorMessage(key, message)
     }
 }
