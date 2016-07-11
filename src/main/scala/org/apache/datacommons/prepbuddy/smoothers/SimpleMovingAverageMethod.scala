c47017a7b71dbcd14279567976ad66546344a824
Lalit Pandey <lalitpan@thoughtworks.com>
2016-07-11 14:52:56 +0530

    refactored smoothers

--- src/main/scala/org/apache/datacommons/prepbuddy/smoothers/SimpleMovingAverageMethod.scala
+++ src/main/scala/org/apache/datacommons/prepbuddy/smoothers/SimpleMovingAverageMethod.scala
@@ -7,10 +7,10 @@ import scala.collection.mutable.ListBuffer
 class SimpleMovingAverageMethod(windowSize: Int) extends SmoothingMethod{
     override def smooth(singleColumnDataset: RDD[String]): RDD[Double] = {
         val duplicateRDD: RDD[Double] = prepare(singleColumnDataset, windowSize)
-        duplicateRDD.mapPartitions((eachIterator) => {
+        duplicateRDD.mapPartitions((eachPartitionsIterator) => {
             val slidingWindow: SimpleSlidingWindow = new SimpleSlidingWindow(windowSize)
             var movingAverage: ListBuffer[Double] = ListBuffer()
-            eachIterator.foreach((eachValue) => {
+            eachPartitionsIterator.foreach((eachValue) => {
                 slidingWindow.add(eachValue)
                 if (slidingWindow.isFull()) {
                     movingAverage += slidingWindow.average
