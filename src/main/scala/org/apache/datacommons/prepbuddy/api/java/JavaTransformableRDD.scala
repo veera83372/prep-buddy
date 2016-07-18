9ba093e980a009a4adffceffdca8595e2fe15cc9
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-07-18 11:48:44 +0530

    Removing the type casting from TRDD methods. Find alternative ways.

--- src/main/scala/org/apache/datacommons/prepbuddy/api/java/JavaTransformableRDD.scala
+++ src/main/scala/org/apache/datacommons/prepbuddy/api/java/JavaTransformableRDD.scala
@@ -8,11 +8,9 @@ import org.apache.datacommons.prepbuddy.normalizers.NormalizationStrategy
 import org.apache.datacommons.prepbuddy.rdds.TransformableRDD
 import org.apache.datacommons.prepbuddy.smoothers.SmoothingMethod
 import org.apache.datacommons.prepbuddy.types.FileType
-import org.apache.spark.api.java.JavaRDD
-import org.apache.spark.rdd.RDD
+import org.apache.spark.api.java.{JavaDoubleRDD, JavaRDD}
 
 import scala.collection.JavaConverters._
-
 
 class JavaTransformableRDD(rdd: JavaRDD[String], fileType: FileType) extends JavaRDD[String](rdd.rdd) {
     private val tRDD: TransformableRDD = new TransformableRDD(rdd.rdd)
@@ -21,10 +19,11 @@ class JavaTransformableRDD(rdd: JavaRDD[String], fileType: FileType) extends Jav
         new JavaTransformableRDD(tRDD.removeRows(rowPurger.evaluate), fileType)
     }
 
-    def deduplicate(primaryKeyColumns: util.List[Integer]): JavaTransformableRDD = {
-        new JavaTransformableRDD(tRDD.deduplicate(primaryKeyColumns.asScala.toList.asInstanceOf[List[Int]]), fileType)
-    }
+    implicit def asScalaList(ls: List[Integer]): List[Int] = ls.map(x => x: Int)
 
+    def deduplicate(primaryKeyColumns: util.List[Integer]): JavaTransformableRDD = {
+        new JavaTransformableRDD(tRDD.deduplicate(primaryKeyColumns.asScala.toList), fileType)
+    }
     def deduplicate: JavaTransformableRDD = new JavaTransformableRDD(tRDD.deduplicate().toJavaRDD(), fileType)
 
     def duplicates(primaryKeyColumns: util.List[Integer]): JavaTransformableRDD = {
@@ -41,8 +40,8 @@ class JavaTransformableRDD(rdd: JavaRDD[String], fileType: FileType) extends Jav
         new JavaTransformableRDD(tRDD.impute(columnIndex, imputationStrategy).toJavaRDD(), fileType)
     }
 
-    def smooth(columnIndex: Int, smoothingMethod: SmoothingMethod): JavaRDD[java.lang.Double] = {
-        tRDD.smooth(columnIndex, smoothingMethod).asInstanceOf[RDD[java.lang.Double]].toJavaRDD()
+    def smooth(columnIndex: Int, smoothingMethod: SmoothingMethod): JavaDoubleRDD = {
+        new JavaDoubleRDD(tRDD.smooth(columnIndex, smoothingMethod).toJavaRDD())
     }
 
     def clusters(columnIndex: Int, clusteringAlgorithm: ClusteringAlgorithm): JavaClusters = {
