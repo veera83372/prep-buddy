e41ea229ab192efe4241b701b14cf6e77e27b64e
Lalit Pandey <lalitpan@thoughtworks.com>
2016-07-25 16:54:57 +0530

    adding documentation for smoothers

--- src/main/scala/org/apache/datacommons/prepbuddy/smoothers/KeyPartitioner.scala
+++ src/main/scala/org/apache/datacommons/prepbuddy/smoothers/KeyPartitioner.scala
@@ -2,6 +2,9 @@ package org.apache.datacommons.prepbuddy.smoothers
 
 import org.apache.spark.Partitioner
 
+/**
+  * A Patitioner that puts each row in a partition that we specify by key.
+  */
 class KeyPartitioner(numOfPartitions: Int) extends Partitioner{
     override def numPartitions: Int = numOfPartitions
 
