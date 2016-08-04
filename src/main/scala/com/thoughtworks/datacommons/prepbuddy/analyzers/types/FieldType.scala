06f93b3876ccfc6ed7d5d9b2eb294a6d72884cc2
Rahul <rahulna@thoughtworks.com>
2016-08-04 18:27:41 +0530

    Modified FieldTypeTest to be more strict and reformatted FieldType Code

--- src/main/scala/com/thoughtworks/datacommons/prepbuddy/analyzers/types/FieldType.scala
+++ src/main/scala/com/thoughtworks/datacommons/prepbuddy/analyzers/types/FieldType.scala
@@ -8,44 +8,22 @@ import scala.util.control.Exception._
 
 object FieldType {
     def inferField(field: String, nullValue: String = ""): DataType = {
-        
-        def tryParseInteger(field: String): DataType = if ((allCatch opt field.toInt).isDefined) {
-            IntegerType
-        } else {
-            tryParseLong(field)
+    
+        def tryParseInt(field: String) = if ((allCatch opt field.toInt).isDefined) IntegerType else tryParseLong(field)
+    
+        def tryParseLong(field: String) = if ((allCatch opt field.toLong).isDefined) LongType else tryParseDouble(field)
+    
+        def tryParseDouble(field: String) = {
+            if ((allCatch opt field.toDouble).isDefined) DoubleType else tryParseTimestamp(field)
         }
-        
-        def tryParseLong(field: String): DataType = if ((allCatch opt field.toLong).isDefined) {
-            LongType
-        } else {
-            tryParseDouble(field)
+    
+        def tryParseTimestamp(field: String) = {
+            if ((allCatch opt Timestamp.valueOf(field)).isDefined) TimestampType else tryParseBoolean(field)
         }
-        
-        def tryParseDouble(field: String): DataType = {
-            if ((allCatch opt field.toDouble).isDefined) {
-                DoubleType
-            } else {
-                tryParseTimestamp(field)
-            }
-        }
-        
-        def tryParseTimestamp(field: String): DataType = {
-            if ((allCatch opt Timestamp.valueOf(field)).isDefined) {
-                TimestampType
-            } else {
-                tryParseBoolean(field)
-            }
-        }
-        
-        def tryParseBoolean(field: String): DataType = {
-            if ((allCatch opt field.toBoolean).isDefined) {
-                BooleanType
-            } else {
-                StringType
-            }
-        }
-        
-        def tryParsing(field: String) = tryParseInteger(field.trim)
+    
+        def tryParseBoolean(field: String) = if ((allCatch opt field.toBoolean).isDefined) BooleanType else StringType
+    
+        def tryParsing(field: String) = tryParseInt(field.trim)
         
         if (field == null || field.isEmpty || field.trim == nullValue) NullType else tryParsing(field.trim)
     }
--- src/test/scala/com/thoughtworks/datacommons/prepbuddy/analyzers/types/FieldTypeTest.scala
+++ src/test/scala/com/thoughtworks/datacommons/prepbuddy/analyzers/types/FieldTypeTest.scala
@@ -6,6 +6,7 @@ import org.scalatest.FunSuite
 class FieldTypeTest extends FunSuite {
     test("String fields types are inferred correctly") {
         assert(FieldType.inferField("test") == StringType)
+        assert(FieldType.inferField("test") != IntegerType)
         assert(FieldType.inferField("12-23-32") == StringType)
         assert(FieldType.inferField("2015-08 14:49:00") == StringType)
         assert(FieldType.inferField("2015-08-20 14") == StringType)
@@ -22,11 +23,13 @@ class FieldTypeTest extends FunSuite {
         assert(FieldType.inferField("3.5") == DoubleType)
         assert(FieldType.inferField("1.0") == DoubleType)
         assert(FieldType.inferField("1.0") == DoubleType)
+        assert(FieldType.inferField("32.322") != LongType)
     }
     
     test("Long fields types are inferred correctly") {
         assert(FieldType.inferField("100000000000") == LongType)
         assert(FieldType.inferField("  492347943247   ") == LongType)
+        assert(FieldType.inferField("492347.943247") != LongType)
     }
     
     test("Null fields are handled properly") {
@@ -36,6 +39,7 @@ class FieldTypeTest extends FunSuite {
         assert(FieldType.inferField(" \t \t ") == NullType)
         assert(FieldType.inferField("") == NullType)
         assert(FieldType.inferField(null) == NullType)
+        assert(FieldType.inferField("x") != NullType)
     }
     
     test("Boolean fields types are inferred correctly") {
@@ -43,10 +47,12 @@ class FieldTypeTest extends FunSuite {
         assert(FieldType.inferField("True ") == BooleanType)
         assert(FieldType.inferField("TRue") == BooleanType)
         assert(FieldType.inferField(" FALSe ") == BooleanType)
+        assert(FieldType.inferField(" ") != BooleanType)
     }
     
     test("Timestamp fields are infered correctly") {
         assert(FieldType.inferField("2015-08-20 14:57:00") == TimestampType)
         assert(FieldType.inferField("2015-08-20 15:57:00") == TimestampType)
+        assert(FieldType.inferField("2015-08-20 15:57:00") != StringType)
     }
 }
