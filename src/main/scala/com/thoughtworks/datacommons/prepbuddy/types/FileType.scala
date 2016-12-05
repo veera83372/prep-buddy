c5353619a0cf76db103cdad377bfa7da611cfcdc
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-12-05 18:07:17 +0530

    Change the csv parsing method in the file type.

--- src/main/scala/com/thoughtworks/datacommons/prepbuddy/types/FileType.scala
+++ src/main/scala/com/thoughtworks/datacommons/prepbuddy/types/FileType.scala
@@ -2,15 +2,39 @@ package com.thoughtworks.datacommons.prepbuddy.types
 
 import com.thoughtworks.datacommons.prepbuddy.utils.RowRecord
 
+import scala.collection.mutable.ListBuffer
+
 /**
   * File formats that are supported by TransformableRDD
   */
 class FileType(delimiter: String) extends Serializable {
+
+    val ESCAPE_SEQUENCE: String = "\""
+
     def appendDelimiter(row: String): String = row + delimiter
 
-    def join(record: RowRecord): String = record.mkString(delimiter)
+    def join(record: RowRecord): String = record.mkString(delimiter) //todo: Need to check delimiter before Joining
 
-    def parse(record: String): RowRecord = new RowRecord(record.split(delimiter, -1).map(_.trim))
+    def parse(record: String): RowRecord = {
+        var parsedRecord: ListBuffer[String] = new ListBuffer[String]()
+        var indexOfDelimiter = 0
+        var escapingCharLength = 1
+        var tempString = record + delimiter
+        while (tempString.length > 0) {
+            if (tempString.startsWith(ESCAPE_SEQUENCE)) {
+                tempString = tempString.tail
+                indexOfDelimiter = tempString.indexOf(ESCAPE_SEQUENCE)
+                escapingCharLength = 2
+            }
+            else {
+                indexOfDelimiter = tempString.indexOf(delimiter)
+                escapingCharLength = 1
+            }
+            parsedRecord += tempString.take(indexOfDelimiter).trim
+            tempString = tempString.substring(indexOfDelimiter + escapingCharLength, tempString.length)
+        }
+        new RowRecord(parsedRecord.toArray)
+    }
 }
 
 object CSV extends FileType(",")
--- src/test/scala/com/thoughtworks/datacommons/prepbuddy/types/FileTypeTest.scala
+++ src/test/scala/com/thoughtworks/datacommons/prepbuddy/types/FileTypeTest.scala
@@ -22,6 +22,17 @@ class FileTypeTest extends FunSuite {
         assert(expected == CSV.join(record))
     }
 
+    test("should be able to parse CSV file and return a RowRecord with comma as data field") {
+        val actual: RowRecord = CSV.parse("6 - 10 years,8.0,\"$40,000 - $50,000\",45000.0")
+
+        val expected: RowRecord = new RowRecord(Array("6 - 10 years","8.0", "$40,000 - $50,000", "45000.0"))
+
+        assert(actual.select(0) == expected.select(0))
+        assert(actual.select(1) == expected.select(1))
+        assert(actual.select(2) == expected.select(2))
+        assert(actual.select(3) == expected.select(3))
+    }
+
     test("should be able to parse TSV file and return a RowRecord") {
         val actual: RowRecord = TSV.parse("Name\t Gender\tAge")
 
