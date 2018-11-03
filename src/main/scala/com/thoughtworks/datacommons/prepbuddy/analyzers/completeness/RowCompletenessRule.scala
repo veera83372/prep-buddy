a761eb4e312316523e241bb343aa98f6b081c1b5
ashishJoy <ashishkujoy@gmail.com>
2018-11-03 22:41:31 +0530

    Handle IndexOutBoundException in case of mandatory column doesn't exist in given row, by throwing column not found exception

--- src/main/scala/com/thoughtworks/datacommons/prepbuddy/analyzers/completeness/RowCompletenessRule.scala
+++ src/main/scala/com/thoughtworks/datacommons/prepbuddy/analyzers/completeness/RowCompletenessRule.scala
@@ -8,32 +8,38 @@ import org.apache.spark.sql.types.StructType
 class RowCompletenessRule(evalMode: EvaluationMode,
                           mandatoryColumns: List[String] = Nil,
                           possibleNullValues: List[String] = Nil) extends Serializable {
-
+    
     if (evalMode == EvaluationMode.CUSTOM && mandatoryColumns == Nil) {
         throw new ApplicationException(ErrorMessages.REQUIREMENT_NOT_MATCHED)
     }
-
+    
     private val nullValues = possibleNullValues ::: List("", "\t")
     private var columnIndexesToConsider: List[Int] = _ //For optimization purpose
-
+    
     def isComplete(record: Row): Boolean = {
         val columnIndexes: List[Int] = getColumnIndexesToConsider(record.schema)
         !columnIndexes
-            .map(record.get)
-            .foldLeft(false)((acc, current) => acc || isNull(current))
+                .map(record.get)
+                .foldLeft(false)((acc, current) => acc || isNull(current))
     }
-
+    
     private def isNull(value: Any): Boolean = {
         value == null || nullValues.contains(value)
     }
-
+    
     private def getColumnIndexesToConsider(schema: StructType): List[Int] = {
         if (columnIndexesToConsider != null) return columnIndexesToConsider
-
+        
         columnIndexesToConsider = if (evalMode == EvaluationMode.STRICT) {
             schema.fieldNames.indices.toList
         } else {
-            mandatoryColumns.map(schema.fieldNames.indexOf(_))
+            mandatoryColumns.map { colName =>
+                if (schema.fieldNames.contains(colName)) {
+                    schema.fieldNames.indexOf(colName)
+                } else {
+                    throw new ApplicationException(ErrorMessages.COLUMN_NOT_FOUND)
+                }
+            }
         }
         columnIndexesToConsider
     }
--- src/test/scala/com/thoughtworks/datacommons/prepbuddy/analyzers/completeness/RowCompletenessRuleTest.scala
+++ src/test/scala/com/thoughtworks/datacommons/prepbuddy/analyzers/completeness/RowCompletenessRuleTest.scala
@@ -1,17 +1,18 @@
 package com.thoughtworks.datacommons.prepbuddy.analyzers.completeness
 
-import com.thoughtworks.datacommons.prepbuddy.exceptions.ApplicationException
+import com.thoughtworks.datacommons.prepbuddy.analyzers.completeness.EvaluationMode.CUSTOM
+import com.thoughtworks.datacommons.prepbuddy.exceptions.{ApplicationException, ErrorMessages}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.scalatest.FunSuite
 
 class RowCompletenessRuleTest extends FunSuite {
     private val spark: SparkSession = SparkSession.builder()
-        .appName(getClass.getCanonicalName)
-        .master("local[2]")
-        .getOrCreate()
-
-
+            .appName(getClass.getCanonicalName)
+            .master("local[2]")
+            .getOrCreate()
+    
+    
     def getPersonsWithSchema: Array[Row] = {
         object Person {
             private val firstName = StructField("firstName", StringType)
@@ -19,10 +20,10 @@ class RowCompletenessRuleTest extends FunSuite {
             private val lastName = StructField("lastName", StringType)
             private val age = StructField("age", IntegerType)
             private val married = StructField("married", BooleanType)
-
+            
             def getSchema: StructType = StructType(Array(firstName, middleName, lastName, age, married))
         }
-
+        
         val data: List[Row] = List(
             Row("John", "-", "-", 28, true),
             Row("Stiven", "", "Smith", null, true),
@@ -33,44 +34,44 @@ class RowCompletenessRuleTest extends FunSuite {
         val personData: DataFrame = spark.createDataFrame(spark.sparkContext.parallelize(data), Person.getSchema)
         personData.collect()
     }
-
+    
     private val persons: Array[Row] = getPersonsWithSchema
-
+    
     private val john = persons(0)
     private val stiven = persons(1)
     private val prasun = persons(2)
     private val ramlal = persons(3)
     private val babulal = persons(4)
-
+    
     test("should return false when the specified column values is/are null") {
         val completenessRule: RowCompletenessRule = new RowCompletenessRule(
-            EvaluationMode.CUSTOM,
+            CUSTOM,
             List("middleName", "age"),
             List("N/A", "-")
         )
-
+        
         assert(!completenessRule.isComplete(john))
         assert(!completenessRule.isComplete(stiven))
         assert(!completenessRule.isComplete(ramlal))
-
+        
         assert(completenessRule.isComplete(prasun))
         assert(completenessRule.isComplete(babulal))
     }
-
+    
     test("should return false when any of the column value is null") {
         val completenessRule: RowCompletenessRule = new RowCompletenessRule(
             EvaluationMode.STRICT,
             possibleNullValues = List("empty", "-")
         )
-
+        
         assert(!completenessRule.isComplete(john))
         assert(!completenessRule.isComplete(stiven))
         assert(!completenessRule.isComplete(ramlal))
         assert(!completenessRule.isComplete(babulal))
-
+        
         assert(completenessRule.isComplete(prasun))
     }
-
+    
     test("should throw exception when Evaluation mode is CUSTOM but mandatory columns are not specified") {
         val resultException: ApplicationException = intercept[ApplicationException] {
             new RowCompletenessRule(
@@ -78,6 +79,18 @@ class RowCompletenessRuleTest extends FunSuite {
                 possibleNullValues = List("empty", "-", "N/A")
             )
         }
-        assert("Requirement did not matched" == resultException.getMessage)
+        assert(ErrorMessages.REQUIREMENT_NOT_MATCHED.getMessage == resultException.getMessage)
+    }
+    
+    test("should throw column not found exception when a mandatory column is not present") {
+        val resultException: ApplicationException = intercept[ApplicationException] {
+            new RowCompletenessRule(
+                EvaluationMode.CUSTOM,
+                mandatoryColumns = List("foo"),
+                possibleNullValues = List("empty", "-", "N/A")
+            ).isComplete(prasun)
+        }
+        
+        assert(ErrorMessages.COLUMN_NOT_FOUND.getMessage == resultException.getMessage)
     }
 }
